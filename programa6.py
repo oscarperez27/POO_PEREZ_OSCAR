28db40bf9ccb5dda07df127e12d51fbe5ce87888
oscarperez27 <leo.hp1@outlook.com>
2024-05-22 19:38:07 -0600

    programa 6 - numero mayor

---
+++ programa6.py
@@ -0,0 +1,9 @@
+x=int(input("Ingresa el primer dato"))
+y=int(input("Ingresa el segundo dato"))
+if x>y:
+    print("El primer dato es mayor que el segundo")
+elif x==y:
+    print("Los datos son iguales")
+else:
+    print("El segundo dato es mayor que el primero")
+
