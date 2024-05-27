34d6f9ceb68b6fdd1cf10c3879e42fb1ca8c1469
oscarperez27 <leo.hp1@outlook.com>
2024-05-26 23:59:34 -0600

    programa 10 - sumas

--- programa10.py
+++ programa10.py
@@ -1,4 +1,6 @@
-x=int(input("Ingresa un valor"))
-y=int(input("Ingresa otro valor"))
+x=None
+y=None
 while x or y != "n":
+    x=int(input("Ingresa un valor"))
+    y=int(input("Ingresa otro valor"))
     print("La suma es", x+y)
